ff6f6228b6a5f9078a5758f8543b70c353972e14
Kade Hemmerling <157063757+kadeanater@users.noreply.github.com>
2024-02-29 15:19:02 -0600

    commit

--- Problem#3/stepCounter.py
+++ Problem#3/stepCounter.py
@@ -1,8 +1,13 @@
 def feet_to_steps(user_feet):
    #write your code here
+    return int(user_feet/ 2.5)
 
 if __name__ == '__main__':
     #take input feet steps here
+    user_feet = float(input())
     #store it into the function
     
+    
+    
     #print your steps here
+    print(feet_to_steps(user_feet))
--- problem#4/intToBin.py
+++ problem#4/intToBin.py
@@ -3,15 +3,16 @@ def int_to_reverse_binary(num1):
 #write your while loop here
     while num1 > 0:
         #write your code
-
+        binary_val += str(num1 % 2)
+        num1 //= 2
     return binary_val;
 
 
 def string_reverse(input_string): 
     reverse_input = ''
-    
    #write your for loop here
-    
+    for char in input_string[::-1]:
+        reverse_input += char
     return reverse_input
 
 if __name__ == '__main__':
